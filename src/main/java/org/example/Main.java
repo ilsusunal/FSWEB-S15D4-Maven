59833369df5e7b54491a9ad1c385c845c46b84b6
ilsu-sunal <sunalilsu@gmail.com>
2024-04-04 17:40:43 +0300

    problemler çözüldü, testleri geçiyor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,47 @@
 package org.example;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Locale;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(checkForPalindrome("I did, did I?"));
+        System.out.println(checkForPalindrome("Was it a car or a cat I saw ?"));
+        System.out.println(checkForPalindrome("hello"));
+        String txt = "Was it a car or a cat I saw ?";
+
+        System.out.println("***********************");
+        System.out.println(convertDecimalToBinary(5));
+        System.out.println(convertDecimalToBinary(6));
+        System.out.println(convertDecimalToBinary(13));
+    }
+    public static boolean checkForPalindrome(String text){
+        String cleanText = text.toLowerCase(Locale.ENGLISH).replaceAll("[^a-zA-Z]", "");
+        int n = cleanText.length();
+        for(int i = 0; i <= n/2; i++){
+            if(cleanText.charAt(i) != cleanText.charAt(n-i-1)){
+                return false;
+            }
+        }
+        return true;
+    }
+//    public static String convertDecimalToBinary(int n){
+//        String binaryString = Integer.toBinaryString(n);
+//        return  binaryString;
+//    }
+    public static String convertDecimalToBinary(int n){
+        LinkedList<Integer> binaryList = new LinkedList<>();
+        while(n > 0){
+            int r = n % 2;
+            binaryList.addFirst(r);
+            n /= 2;
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int number : binaryList){
+            sb.append(number);
+        }
+        return  sb.toString();
     }
 
 }
